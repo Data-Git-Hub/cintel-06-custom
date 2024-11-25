9934d47a905f4fb4392cbc94bb109fc073a5bf29
Data Analytics Graduate Student <s576977@nwmissouri.edu>
2024-11-25 16:48:35 -0600

    Update app.py
    P6.1 Base

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+from pathlib import Path
+
+import pandas
+
+from shiny import reactive
+from shiny.express import render, ui
+
+
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "USDA_75_23_CPI.csv"
+    return pandas.read_csv(infile)
+
+
+with ui.navset_card_underline():
+
+    with ui.nav_panel("Graphics"):
+
+        @render.data_frame
+        def frame():
+            # Give dat() to render.DataGrid to customize the grid
+            return dat()
+
+    with ui.nav_panel("Table"):
+
+        @render.table
+        def table():
+            return dat()
 
